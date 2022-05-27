3c4ade3ad0f0d1f326eb8c4b52ff5184bfa3cd6f
Josh Thompson <thompsonjoshd@gmail.com>
2022-05-27 03:01:09 -0600

    migrate pgcrypto enable_extension

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_27_085839) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_27_090016) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "mobility_network_users", force: :cascade do |t|
