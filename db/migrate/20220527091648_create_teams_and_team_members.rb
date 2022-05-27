21bd83c2d001fd147afa4d29cdf61a4f25008eda
Josh Thompson <thompsonjoshd@gmail.com>
2022-05-27 03:28:25 -0600

    teams and team members, ui: :uuid

--- db/migrate/20220527091648_create_teams_and_team_members.rb
+++ db/migrate/20220527091648_create_teams_and_team_members.rb
@@ -1,6 +1,6 @@
 class CreateTeamsAndTeamMembers < ActiveRecord::Migration[7.0]
   def change
-    create_table :teams do |t|
+    create_table :teams, id: :uuid do |t|
       t.string :name
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_27_090801) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_27_091648) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -19,6 +19,21 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_27_090801) do
     t.string "name"
     t.string "strava_athlete_id"
     t.string "email_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "team_members", force: :cascade do |t|
+    t.bigint "team_id"
+    t.bigint "mobility_network_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mobility_network_user_id"], name: "index_team_members_on_mobility_network_user_id"
+    t.index ["team_id"], name: "index_team_members_on_team_id"
+  end
+
+  create_table "teams", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
